ea1e7ec38258f056709fb38ad57d3ef52aad85e0
ratelhugo <ratelhugo@gmail.com>
2019-02-07 17:15:03 +0100

    vues conseil + entrainement avec d'un actionsheet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' }
+
 ];
 @NgModule({
   imports: [
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+
+export class Tab3Page {
+    constructor(public actionSheetController: ActionSheetController) {}
+
+    async testactionsheet() {
+        const actionSheet = await this.actionSheetController.create({
+            header: 'Albums',
+            buttons: [{
+                text: 'Supprimer',
+                role: 'destructive',
+                icon: 'trash',
+                handler: () => {
+                    console.log('Delete clicked');
+                }
+            }, {
+                text: 'Ajouter',
+                icon: 'share',
+                handler: () => {
+                    console.log('Share clicked');
+                }
+            }, {
+                text: 'Cancel',
+                icon: 'close',
+                role: 'cancel',
+                handler: () => {
+                    console.log('Cancel clicked');
+                }
+            }]
+        });
+        await actionSheet.present();
+    }
+}
